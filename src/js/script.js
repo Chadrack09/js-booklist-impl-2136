7ddab36faf3b81fcaef03239adcba7597a3fbfd5
Chadrack <bbenolych@gmail.com>
2021-04-01 09:41:38 +0200

    files added

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+// Book Class
+class Book {
+  constructor (title, author, isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+
+  // Methods that display Book
+  static displayBook(){
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const form = document.querySelector('#form');
+
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+//   const title = document.querySelector('#title').value;
+//   const author = document.querySelector('#author').value;
+//   const isbn = document.querySelector('#isbn').value;
+//   const row = document.createElement('tr');
+//   const rData = document.createElement('td');
+//   rData.innerHTML = `${title}`;
+//   row.appendChild(rData);
+//   console.log(row);
+// });
