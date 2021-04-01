63b8af3112e104c7f435d698d7d2f7fb243c4134
Chadrack Boudzoumou <52974721+Chadrack09@users.noreply.github.com>
2021-04-01 11:39:39 +0200

    commiting the script (#1)
    I am adding your pull request since I have not found any conflits

--- src/js/script.js
+++ src/js/script.js
@@ -14,16 +14,3 @@ class Book {
     return `This Book: ${this.title} was written by ${this.author}`;
   }
 }
-
-const form = document.querySelector('#form');
-// form.addEventListener('submit', e => {
-//   e.preventDefault();
-//   const title = document.querySelector('#title').value;
-//   const author = document.querySelector('#author').value;
-//   const isbn = document.querySelector('#isbn').value;
-//   const row = document.createElement('tr');
-//   const rData = document.createElement('td');
-//   rData.innerHTML = `${title}`;
-//   row.appendChild(rData);
-//   console.log(row);
-// });
